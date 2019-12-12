ecdfc2c6a6f1249c6a148892f59edf11470c7f3e
labuser <labuser@course.local>
2019-12-12 15:11:31 +0530

    changes done on loginservlet

--- myRegister/src/com/loginServlet.java
+++ myRegister/src/com/loginServlet.java
@@ -40,7 +40,7 @@ public class loginServlet extends HttpServlet {
 		{ 
 		 String name1=request.getParameter("name");
 	     String password1=request.getParameter("password");
-	     
+	     System.out.println("pawar saheb`s birthday today")
 	    Connection con= JDBCLoginService.connectionMethod();
 	    PreparedStatement st=con.prepareStatement("select * from user where id=(?) and password=(?)");
 	    st.setString(1, name1);
