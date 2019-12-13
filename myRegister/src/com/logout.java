f82172823d09643a93012f1f525ee0538c7c07a4
kiranjadhav-01 <kiranbjadhav1234@gmail.com>
2019-12-13 01:04:26 -0800

    changes done

--- myRegister/src/com/logout.java
+++ myRegister/src/com/logout.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class logout
  */
-@WebServlet("/loggggggggggggggg")
+@WebServlet("/logout")
 public class logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
